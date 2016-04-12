eb3d6b4fb5457c95d894aeaa38ee48e212d4c1c4
HiShan <39868676@qq.com>
2016-04-12 21:39:14 +0800

    restart

---
+++ bin/himan.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+require('commander')
+    .version('1.0.0')
+    .usage('<command> [options]')
+    .command('init', 'start')
+    .command('clone', 'copy node_modules')
+    .parse(process.argv)
---
+++ resources/$config.js
@@ -0,0 +1,8 @@
+module['exports'] = {
+    path : {
+        dev : 'src',
+        dev_server:  '.tmp',
+        pre : 'pre',
+        prod : 'dist',
+    }
+}
