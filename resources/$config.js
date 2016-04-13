b31a1124f3490d4da12b9938f17a8183d4f1c312
HiShan <39868676@qq.com>
2016-04-13 17:49:21 +0800

    init

--- resources/$config.js
+++
@@ -1,8 +0,0 @@
-module['exports'] = {
-    path : {
-        dev : 'src',
-        dev_server:  '.tmp',
-        pre : 'pre',
-        prod : 'dist',
-    }
-}
--- resources/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    task = require('hi-gulp4-cfg'),
-    dev = task.dev(),
-    prod = task.prod()
-;
-
-gulp.task('default', dev);
-gulp.task('dist', prod)
